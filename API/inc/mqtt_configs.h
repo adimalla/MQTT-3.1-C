fbf7fa2289a4e00e9828d2a3bda400576b0a5b64
adityamall <aditya.mall@gmail.com>
2019-08-31 02:05:52 -0500

    config file added

---
+++ API/inc/mqtt_configs.h
@@ -0,0 +1,46 @@
+/**
+ ******************************************************************************
+ * @file    mqtt_configs.h, file name will change
+ * @author  Aditya Mall,
+ * @brief   Example MQTT publish client, for mosquitto MQTT Broker
+ *
+ *  Info
+ *          API
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2019 Aditya Mall </center></h2>
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
+#ifndef MQTT_CONFIGS_H_
+#define MQTT_CONFIGS_H_
+
+
+
+/******************************************************************************/
+/*                                                                            */
+/*                  MQTT Message Macros and Defines                           */
+/*                                                                            */
+/******************************************************************************/
+
+
+
+
+
+
+
+
+#endif /* INC_MQTT_CONFIGS_H_ */
