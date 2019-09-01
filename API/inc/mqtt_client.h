4e493765c4ee9846e9f135a047aa7f8e215a5667
root <aditya.mall@gmail.com>
2019-09-01 06:01:51 -0500

    mqtt client handler structure added

--- API/inc/mqtt_client.h
+++ API/inc/mqtt_client.h
@@ -33,7 +33,7 @@
  * Standard Header and API Header files
  */
 #include <stdint.h>
-
+#include "mqtt_configs.h"
 
 
 
@@ -48,14 +48,13 @@
 /* @brief MQTT Fixed Header, common to all control messages */
 typedef struct mqtt_header
 {
-    uint8_t retain_flag     : 1;  /*!< Message Retain Flag (LSB)                                               */
-    uint8_t qos_level       : 2;  /*!< Quality of service Level (LSB)                                          */
-    uint8_t dup_flag        : 1;  /*!< Duplicate Flag (LSB)                                                    */
-	uint8_t message_type    : 4;  /*!< Message Type (MSB)                                                      */
+    uint8_t retain_flag     : 1;  /*!< Message Retain Flag, (LSB)                                              */
+    uint8_t qos_level       : 2;  /*!< Quality of service Level, (LSB)                                         */
+    uint8_t dup_flag        : 1;  /*!< Duplicate Flag, (LSB)                                                   */
+	uint8_t message_type    : 4;  /*!< MQTT Control Packet / Message Type, (MSB)                               */
 	uint8_t message_length;       /*!< Length of MQTT Message (Remaining Length, excludes fixed header length) */
 
 }mqtt_header_t;
-
 
 
 /* @brief MQTT CONNECT structures */
@@ -66,27 +65,40 @@ typedef struct mqtt_connect_flags
 	uint8_t reserved        : 1;
 	uint8_t clean_session   : 1;
 	uint8_t will_flag       : 1;
-	uint8_t will_qos        : 1;
+	uint8_t will_qos        : 2;
 	uint8_t will_retain     : 1;
 	uint8_t password_flag   : 1;
 	uint8_t username_flag   : 1;
 
 }mqtt_connect_flags_t;
 
-
+#pragma pack(1)
 /* Main MQTT Connect Structure */
 typedef struct mqtt_connect
 {
-	mqtt_header_t        fixed_header;          /*!< MQTT Fixed Header         */
-	uint16_t             protocol_name_length;  /*!< MQTT Protocol Name Length */
-	char                 protocol_name[6];      /*!< MQTT Protocol Name        */
-	uint8_t              protocol_version;      /*!< MQTT Protocol Version     */
-	mqtt_connect_flags_t connect_flags;         /*!< Connect Message Flags     */
-	uint16_t             keep_alive_value;      /*!< Client Keep Alive Value   */
-	uint16_t             client_id_length;      /*!< Client ID Length          */
-	char                 client_id[23];         /*!< Client ID                 */
+	mqtt_header_t        fixed_header;                         /*!< MQTT Fixed Header                              */
+	uint16_t             protocol_name_length;                 /*!< MQTT Protocol Name Length                      */
+	char                 protocol_name[PROTOCOL_NAME_LENGTH];  /*!< MQTT Protocol Name,length defined in configs.h */
+	uint8_t              protocol_version;                     /*!< MQTT Protocol Version                          */
+	mqtt_connect_flags_t connect_flags;                        /*!< Connect Message Flags                          */
+	uint16_t             keep_alive_value;                     /*!< Client Keep Alive Value                        */
+	uint16_t             client_id_length;                     /*!< Client ID Length                               */
+	char                 client_id[CLIENT_ID_LENGTH];          /*!< Client ID, length defined in config.h          */
 
 }mqtt_connect_t;
+
+
+
+
+typedef struct mqtt_client
+{
+	mqtt_connect_t *connect_msg;
+	//mqtt_connack_t *connack_msg;
+	//mqtt_publish_t *publish_msg;
+
+	size_t mqtt_message_length;
+
+}mqtt_client_t;
 
 
 
@@ -98,6 +110,8 @@ typedef struct mqtt_connect
 /*                                                                            */
 /******************************************************************************/
 
+
+size_t mqtt_connect(mqtt_client_t *client, char *client_name);
 
 
 #endif /* MQQT_CLIENT_H_ */
