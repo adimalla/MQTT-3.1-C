1d4d11867f758a2e4479cb1de6126d345b0b075e
root <aditya.mall@gmail.com>
2019-08-31 21:49:21 -0500

    static mqtt htons function added

--- API/src/mqqt_pub.c
+++ API/src/mqqt_pub.c
@@ -1,8 +1,57 @@
-/*
- * mqqt_pub.c
+/**
+ ******************************************************************************
+ * @file    pub_mqtt.c, file name will change
+ * @author  Aditya Mall,
+ * @brief   Example MQTT publish client, for mosquitto MQTT Broker
  *
- *  Created on: Aug 29, 2019
- *      Author: adityamall
+ *  Info
+ *          Only for testing, (POSIX compatible only.)
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2019 Aditya Mall </center></h2>
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
  */
+
+
+/*
+ * Standard Header and API Header files
+ */
+#include <stdint.h>
+#include "mqtt_configs.h"
+#include "mqtt_client.h"
+
+
+
+static uint16_t mqtt_htons(uint16_t value)
+{
+	value  = ((value & 0xFF00) >> 8) + ((value & 0x00FF) << 8);
+
+	return value;
+}
+
+
+int8_t mqtt_connect(mqtt_connect_t *connect_msg)
+{
+
+	connect_msg->fixed_header.message_type = 0x01;     //fixed
+
+	return 0;
+}
+
+
 
 
